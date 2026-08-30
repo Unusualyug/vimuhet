cfefbfadd157d80c2dab83ffba721becc8a37025
Yug Lakhani <yuglakhani34@gmail.com>
2026-08-30 14:51:41 +0530

    Add sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,12 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://vimuhet.vercel.app/",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://vimuhet.vercel.app/shop",
+      lastModified: new Date(),
+    },
+  ];
+}
